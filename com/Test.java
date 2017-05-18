b059cbc60470a271dc4808b06dc2d3f891ddd138
qiming.li <qiming.li@baozun.cn>
2017-05-18 14:27:51 +0800

    测试

--- com/Test.java
+++ com/Test.java
@@ -1,12 +1,7 @@
 
 public class Test {
 
-<<<<<<< HEAD
- String aaabbb;
- String bbaadd;
- String aaa111;
-=======
->>>>>>> branch 'test' of git@github.com:qmlid/test.git
+
  public static void main(String[] args) {
 	
 }
