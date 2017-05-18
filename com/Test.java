e3c37f0b972edd979fcca1f35a06a4d0f7541249
qiming.li <qiming.li@baozun.cn>
2017-05-18 14:20:14 +0800

    删除属性

--- com/Test.java
+++ com/Test.java
@@ -1,12 +1,8 @@
 
 public class Test {
 
-<<<<<<< HEAD
- String aaabbb;
- String bbaadd;
- String aaa111;
-=======
->>>>>>> branch 'test' of git@github.com:qmlid/test.git
+
+
  public static void main(String[] args) {
 	
 }
